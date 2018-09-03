ad501a88e9f4728264c1b92d37dfa8252c191734
Jorgen Evens <jorgen@evens.eu>
2018-09-03 21:35:10 +0200

    Initial checkin

---
+++ README.md
@@ -0,0 +1,28 @@
+# swagdoc
+
+[![CircleCI](https://circleci.com/gh/jorgenevens/swagdoc/tree/master.svg?style=shield)](https://circleci.com/gh/jorgenevens/swagdoc/tree/master)
+[![Version](https://img.shields.io/node/v/@jorgenevens/swagdoc.svg)](https://npmjs.org/@jorgenevens/swagdoc)
+
+## Usage
+
+Using `npx`
+
+```sh
+npx @jorgenevens/swagdoc
+```
+
+Or by installing the package globally
+
+```
+npm install -g @jorgenevens/swagdoc
+
+swagdoc [path/to/specs]
+```
+
+## Contributing
+
+I really appreciate any contribution you would like to make, so don't hesitate to report an issue or submit pull requests.
+
+## About me
+
+Hi, my name is [Jorgen Evens](https://jorgen.evens.eu). By day I built things (mainly in PHP and JavaScript) for [Ambassify](https://ambassify.com) and by night I tinker around with these kinds of projects.
---
+++ swagger-docs.js
@@ -0,0 +1,102 @@
+#!/usr/bin/env node
+
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const URL = require('url');
+
+const open = require('open');
+const express = require('express');
+const serveIndex = require('serve-index');
+
+const EDITOR = path.join(__dirname, './static/index.html');
+const SOURCE = path.resolve(process.cwd(), process.argv[2] || '.');
+const SWAGGER_EDITOR = path.dirname(require.resolve('swagger-editor-dist'));
+const EMPTY_FILE = fs.readFileSync(path.resolve(__dirname, './static/empty-spec.yaml'));
+
+function isFile(file) {
+    try {
+        if (!fs.statSync(file).isFile())
+            throw new Error('not a file');
+
+        return true;
+    } catch(e) {
+        return false;
+    }
+}
+
+
+const app = express();
+const server = http.createServer(app);
+
+// save the file from swagger-editor
+app.put('/editor/spec', function (req, res, next) {
+    var file = path.join(SOURCE, req.query.file);
+
+    if (!isFile(file)) {
+        return next();
+    }
+
+    var stream = fs.createWriteStream(file);
+    req.pipe(stream);
+    stream.on('finish', function () {
+        res.end('ok');
+    });
+
+});
+
+// retrieve the project swagger file for the swagger-editor
+app.get('/editor/spec', function(req, res, next) {
+    var file = path.join(SOURCE, req.query.file);
+
+    if (!isFile(file))
+        return next();
+
+    fs.createReadStream(file).pipe(res);
+});
+
+// serve references
+app.use('/ref', express.static(SOURCE));
+
+app.get('*.yaml', function(req, res) {
+    var url = URL.parse(req.url);
+    var file = path.join(SOURCE, url.pathname);
+
+    if (!isFile(file) && !req.query.create) {
+        return res.type('text/html').end(`
+            <html>
+                <body>
+                    <p>
+                        Would you like to
+                        <a href="${url.pathname}?create=1">create</a>
+                        the file?
+                    </p>
+
+                    <p>Go <a href="#" onclick="history.back()">Back</a></p>
+                </body>
+            </html>`);
+    }
+
+    if (!isFile(file))
+        fs.writeFileSync(file, EMPTY_FILE);
+
+    fs.createReadStream(EDITOR).pipe(res);
+});
+
+// serve source files
+app.use('/', express.static(SOURCE), serveIndex(SOURCE));
+
+// serve swagger-editor
+app.use('/', express.static(SWAGGER_EDITOR));
+
+server.listen(process.env.PORT || 0, 'localhost', function () {
+    const { port } = server.address();
+    const url = `http://localhost:${port}/`;
+
+    /* eslint-disable no-console */
+    console.error('Starting Swagger editor.');
+    console.error('URL:', url);
+    /* eslint-enable */
+    open(url);
+
+});
